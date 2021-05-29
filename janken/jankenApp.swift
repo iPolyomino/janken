aa4b2a4adb8ba0a96603e8354783b8910c856b2b
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-05-29 15:48:06 +0900

    Initial Commit

---
+++ janken/jankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  jankenApp.swift
+//  janken
+//
+//  Created by 萩倉丈 on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct jankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
